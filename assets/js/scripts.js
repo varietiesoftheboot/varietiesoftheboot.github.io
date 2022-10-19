02de4e8351a5dd832990e3afa3f9bfc4cb95d1dc
alanramponi <alramponi@fbk.eu>
2022-10-19 11:20:12 +0200

    Fixed menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,23 @@
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+var anchorTrigger = document.querySelector('#main-menu-mobile ul li a');
+var anchorTrigger2 = document.querySelector('#main-menu-mobile ul li:nth-of-type(2) a');
 
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
+}
+
+anchorTrigger.onclick = function() {
+    menuContainer.classList.remove('open');
+    menuTrigger.classList.remove('is-active')
+    body.classList.remove('lock-scroll')
+}
+
+anchorTrigger2.onclick = function() {
+    menuContainer.classList.remove('open');
+    menuTrigger.classList.remove('is-active')
+    body.classList.remove('lock-scroll')
 }
